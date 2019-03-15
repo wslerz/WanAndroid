0f0681c57b95dd80f10cdf06aac1432b84a8d9b4
wslerz <827203144@qq.com>
2019-03-15 10:46:51 +0800

    添加api完成

--- app/src/main/java/com/wslerz/wanandroid/http/service/ApiService.java
+++ app/src/main/java/com/wslerz/wanandroid/http/service/ApiService.java
@@ -233,11 +233,190 @@ public interface ApiService {
      * @return 收藏站外文章
      */
     @POST("lg/collect/add/json")
+    @FormUrlEncoded
     Flowable<BaseResponse<BaseEntity>> collectOut(@Field("title") String title,
                                                   @Field("author") String author,
                                                   @Field("link") String link);
 
 
+    /**
+     * 取消收藏 文章列表
+     * https://www.wanandroid.com/lg/uncollect_originId/2333/json
+     *
+     * @param id 表中文章的id
+     * @return 取消收藏
+     */
+    @POST("lg/uncollect_originId/{id}/json")
+    Flowable<BaseResponse<BaseEntity>> unCollectOriginId(@Path("id") int id);
 
 
+    /**
+     * 取消收藏 我的收藏页面（该页面包含自己录入的内容）
+     * https://www.wanandroid.com/lg/uncollect/2805/json
+     *
+     * @param id       id:拼接在链接上
+     * @param originId originId:列表页下发，无则为-1 originId 代表的是你收藏之前的那篇文章本身的id； 但是收藏支持主动添加，这种情况下，没有originId则为-1
+     * @return 取消收藏
+     */
+    @POST("lg/uncollect/{id}/json")
+    @FormUrlEncoded
+    Flowable<BaseResponse<BaseEntity>> unCollect(@Path("id") int id,
+                                                 @Field("originId") int originId);
+
+    /**
+     * 6.5 收藏网站列表
+     * https://www.wanandroid.com/lg/collect/usertools/json
+     *
+     * @return 收藏网站列表
+     */
+    @GET("lg/collect/usertools/json")
+    Flowable<BaseResponse<BaseEntity>> getCollectUserTool();
+
+    /**
+     * 收藏网址
+     * https://www.wanandroid.com/lg/collect/addtool/json
+     *
+     * @param name name
+     * @param link link
+     * @return 收藏网址
+     */
+    @POST("lg/collect/addtool/json")
+    @FormUrlEncoded
+    Flowable<BaseResponse<BaseEntity>> addCollectUserTool(@Field("name") String name,
+                                                          @Field("link") String link);
+
+    /**
+     * 编辑收藏网站
+     * https://www.wanandroid.com/lg/collect/updatetool/json
+     *
+     * @param id   id
+     * @param name name
+     * @param link link
+     * @return 编辑收藏网站
+     */
+    @POST("lg/collect/updatetool/json")
+    @FormUrlEncoded
+    Flowable<BaseResponse<BaseEntity>> updateCollectUserTool(@Field("id") int id,
+                                                             @Field("name") String name,
+                                                             @Field("link") String link);
+
+    /**
+     * 删除收藏网站
+     * https://www.wanandroid.com/lg/collect/deletetool/json
+     *
+     * @param id id
+     * @return 删除收藏网站
+     */
+    @POST("lg/collect/deletetool/json")
+    @FormUrlEncoded
+    Flowable<BaseResponse<BaseEntity>> deleteCollectUserTool(@Field("id") int id);
+
+    /**
+     * 搜索
+     * https://www.wanandroid.com/article/query/0/json
+     *
+     * @param pageNo 页码：拼接在链接上，从0开始。
+     * @param k      搜索关键词
+     * @return 搜索
+     */
+    @POST("article/query/{pageNo}/json")
+    @FormUrlEncoded
+    Flowable<BaseResponse<BaseEntity>> search(@Path("pageNo") int pageNo,
+                                              @Field("k") String k);
+
+
+    /**
+     * 新增一个 TODO
+     * https://www.wanandroid.com/blog/show/2442
+     * http://www.wanandroid.com/lg/todo/add/json
+     * <p>
+     * title: 新增标题（必须）
+     * content: 新增详情（必须）
+     * date: 2018-08-01 预定完成时间（不传默认当天，建议传）
+     * type: 大于0的整数（可选）；
+     * type 可以用于，在app 中预定义几个类别：例如 工作1；
+     * 生活2；娱乐3；新增的时候传入0，1，2，查询的时候，传入type 进行筛选；
+     * priority 大于0的整数（可选）；
+     * priority 主要用于定义优先级，在app 中预定义几个优先级：重要（1），
+     * 一般（2）等，查询的时候，传入priority 进行筛选；
+     *
+     * @param title    标题
+     * @param content  详情
+     * @param date     预定完成时间
+     * @param type     类型
+     * @param priority 重要度
+     * @return 新增一个 TODO
+     */
+    @POST("lg/todo/add/json")
+    @FormUrlEncoded
+    Flowable<BaseResponse<BaseEntity>> addTodo(@Field("title") String title,
+                                               @Field("content") String content,
+                                               @Field("date") String date,
+                                               @Field("type") int type,
+                                               @Field("priority") int priority);
+
+
+    /**
+     * 更新一个 Todo
+     *
+     * @param id       拼接在链接上，为唯一标识，列表数据返回时，每个todo 都会有个id标识 （必须）
+     * @param title    更新标题 （必须）
+     * @param content  新增详情（必须）
+     * @param date     2018-08-01（必须）
+     * @param type     0 // 0为未完成，1为完成
+     * @param status   status
+     * @param priority priority
+     * @return
+     */
+    @POST("lg/todo/update/{id}/json")
+    @FormUrlEncoded
+    Flowable<BaseResponse<BaseEntity>> updateTode(@Path("id") int id,
+                                                  @Field("title") String title,
+                                                  @Field("content") String content,
+                                                  @Field("date") String date,
+                                                  @Field("type") int type,
+                                                  @Field("status") int status,
+                                                  @Field("priority") int priority);
+
+    /**
+     * 删除一个 Todo
+     * http://www.wanandroid.com/lg/todo/delete/83/json
+     *
+     * @param id id
+     * @return 删除一个
+     */
+    @POST("lg/todo/delete/{id}/json")
+    @FormUrlEncoded
+    Flowable<BaseResponse<BaseEntity>> deleteTode(@Path("id") int id);
+
+    /**
+     * 仅更新完成状态Todo
+     * http://www.wanandroid.com/lg/todo/done/80/json
+     * 只会变更status，未完成->已经完成 or 已经完成->未完成。
+     *
+     * @param id     id: 拼接在链接上，为唯一标识
+     * @param status 0或1，传1代表未完成到已完成，反之则反之。
+     * @return 仅更新完成状态Todo
+     */
+    @POST("lg/todo/done/{id}/json")
+    Flowable<BaseResponse<BaseEntity>> updateTodeStatus(@Path("id") int id,
+                                                        @Field("status") int status);
+
+    /**
+     * http://www.wanandroid.com/lg/todo/v2/list/页码/json
+     * TODO 列表
+     *
+     * @param pageNo   页码从1开始，拼接在url 上
+     * @param status   status 状态， 1-完成；0未完成; 默认全部展示；
+     * @param type     type 创建时传入的类型, 默认全部展示
+     * @param priority priority 创建时传入的优先级；默认全部展示
+     * @param orderby  orderby 1:完成日期顺序；2.完成日期逆序；3.创建日期顺序；4.创建日期逆序(默认)；
+     * @return
+     */
+    @GET("lg/todo/v2/list/{pageNo}/json")
+    Flowable<BaseResponse<BaseEntity>> getTodeList(@Path("pageNo") int pageNo,
+                                                   @Query("status") int status,
+                                                   @Query("type") int type,
+                                                   @Query("priority") int priority,
+                                                   @Query("orderby") int orderby);
 }
